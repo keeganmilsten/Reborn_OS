47a5ec2a3993485f9a148c9e61377f6ff83d95fc
Keegan <willnhalt@gmail.com>
2017-10-07 10:23:34 -0400

    Update customize_airootfs.sh

--- airootfs/root/customize_airootfs.sh
+++ airootfs/root/customize_airootfs.sh
@@ -20,4 +20,4 @@ sed -i 's/#\(HandleHibernateKey=\)hibernate/\1ignore/' /etc/systemd/logind.conf
 sed -i 's/#\(HandleLidSwitch=\)suspend/\1ignore/' /etc/systemd/logind.conf
 
 systemctl enable pacman-init.service choose-mirror.service
-systemctl set-default multi-user.target
+systemctl set-default graphical.target
