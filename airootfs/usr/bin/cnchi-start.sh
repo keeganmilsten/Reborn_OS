9a7c86221fd8cbea33fa0492b85aed13909e5f2d
Keegan <willnhalt@gmail.com>
2017-11-15 06:10:03 -0500

    Update cnchi-start.sh

--- airootfs/usr/bin/cnchi-start.sh
+++ airootfs/usr/bin/cnchi-start.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-sudo -E /usr/bin/python -Wall /usr/share/cnchi/src/cnchi.py -dvz --disable-update
+sudo -E /usr/bin/python -Wall /usr/share/cnchi/src/cnchi.py -dvz --disable-update --packagelist /usr/share/cnchi/data/packages.xml
