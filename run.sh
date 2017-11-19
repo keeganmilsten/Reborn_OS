3791a412888433d83c475a0a4b0873d3313b24d2
Keegan <willnhalt@gmail.com>
2017-11-19 09:08:48 -0500

    Create run.sh

---
+++ run.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/bash
+echo "Correcting file permissions"
+chmod +x ${work_dir}/Cnchi/postinstall.sh
+chmod +x ${work_dir}/build.sh
+chmod +x ${work_dir}/translations.sh
+chmod +x ${work_dir}/airootfs/etc/skel/.config/autostart/internet.sh
+chmod +x ${work_dir}/airootfs/etc/systemd/scripts/choose-mirror
+chmod +x ${work_dir}/airootfs/root/.automated_script.sh
+chmod +x ${work_dir}/airootfs/root/customize_airootfs.sh
+chmod +x ${work_dir}/airootfs/usr/bin/cnchi-start.sh
+chmod +x ${work_dir}/airootfs/usr/bin/internet.sh
+echo "DONE"
