9a07d0f098383c654f931dfaf7892384fef7d4c1
Keegan <willnhalt@gmail.com>
2017-11-21 15:41:18 -0500

    Update postinstall.sh

--- Cnchi/postinstall.sh
+++ Cnchi/postinstall.sh
@@ -466,6 +466,9 @@ fi
     
     # Copy correct pacman.conf file over
     cp /etc/pacman.conf ${CN_DESTDIR}/etc/
+    # Fix Grub
+    grub-mkconfig -o ${CN_DESTDIR}/boot/grub/grub.cfg
+    echo "GRUB FIXED"
 }
 
 touch /tmp/.postinstall.lock
