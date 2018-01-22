72b6d3d46174598280554ac400d7a69c03b748e4
Keegan <willnhalt@gmail.com>
2018-01-22 22:05:47 +0000

    Update flatpak.sh

--- Cnchi/flatpak.sh
+++ Cnchi/flatpak.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 # -*- coding: utf-8 -*-
 #
-flatpak=`flatpak remote-add flathub https://dl.flathub.org/repo/flathub.flatpakrepo`
 
         yad --title "Flatpak" --height=100 --width=300 --center --buttons-layout=center \
         --text="Click the button below to get started using flatpak packages. These are extra packages that are distro independent, adding more available packages to your system." --text-align=center \
-        --button="Add Flatpak Repository":$flatpak \
+        --button="Add Flatpak Repository":pkcon.sh \
         --button="Not Now":1
