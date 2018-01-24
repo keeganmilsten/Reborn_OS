9c5c1c002d935d51e0efd1b308b13bfc6b0d22d7
Keegan <willnhalt@gmail.com>
2018-01-24 15:59:25 -0500

    Update flatpak.sh

--- Cnchi/flatpak.sh
+++ Cnchi/flatpak.sh
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 #
 
-        yad --title "Flatpak" --height=100 --width=300 --center --buttons-layout=center \
+        yad --image="gtk-help" --title "Flatpak" --height=100 --width=300 --center --buttons-layout=center \
         --text="Click the button below to get started using flatpak packages. These are extra packages that are distro independent, adding more available packages to your system." --text-align=center \
         --button="Add Flatpak Repository":pkcon.sh \
-        --button="Not Now":1
+        --button="Remove From Startup":pkcon2.sh
