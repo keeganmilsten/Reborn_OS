97fc4fc78516af98f124de3fc72ab53f6df5f8b1
Keegan <willnhalt@gmail.com>
2018-01-25 08:01:06 -0500

    Update pkcon.sh

--- Cnchi/pkcon.sh
+++ Cnchi/pkcon.sh
@@ -5,4 +5,12 @@ pkcon refresh
 
 pkexec rm /usr/share/applications/flatpak.desktop
 
+if [ -f /usr/bin/gnome-software ]; then
 gnome-software
+echo "STARTING GNOME-SOFTWARE"
+fi
+
+if [ -f /usr/bin/plasma-discover ]; then
+plasma-discover
+echo "STARTING PLASMA-DISCOVER"
+fi
