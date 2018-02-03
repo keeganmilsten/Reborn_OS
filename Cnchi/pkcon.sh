5e8a6b3e609365dc696f0662b7caf532e35c93a3
Keegan <willnhalt@gmail.com>
2018-02-02 21:15:53 -0500

    Update pkcon.sh

--- Cnchi/pkcon.sh
+++ Cnchi/pkcon.sh
@@ -1,3 +1,5 @@
+wget --spider www.google.com
+if [ "$?" = 0 ]; then
 
 flatpak remote-add flathub https://dl.flathub.org/repo/flathub.flatpakrepo
 
@@ -13,4 +15,7 @@ fi
 if [ -f /usr/bin/plasma-discover ]; then
 plasma-discover
 echo "STARTING PLASMA-DISCOVER"
+fi
+
+else exec /usr/bin/pkcon.sh
 fi
