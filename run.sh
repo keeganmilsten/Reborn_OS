1e71ea6e7a51d22d5108b204f57dd89397b521a0
Keegan <willnhalt@gmail.com>
2018-02-26 06:07:19 -0500

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/bash
 echo "Correcting file permissions"
-chmod +x $PWD/Cnchi/postinstall.sh
-chmod +x $PWD/Cnchi/10_antergos
-chmod +x $PWD/build.sh
-chmod +x $PWD/translations.sh
-chmod +x $PWD/airootfs/etc/skel/.config/autostart/internet.sh
-chmod +x $PWD/airootfs/etc/systemd/scripts/choose-mirror
-chmod +x $PWD/airootfs/root/.automated_script.sh
-chmod +x $PWD/airootfs/root/customize_airootfs.sh
-chmod +x $PWD/airootfs/usr/bin/cnchi-start.sh
-chmod +x $PWD/airootfs/usr/bin/internet.sh
+sudo chmod +x $PWD/Cnchi/postinstall.sh
+sudo chmod +x $PWD/Cnchi/10_antergos
+sudo chmod +x $PWD/build.sh
+sudo chmod +x $PWD/translations.sh
+sudo chmod +x $PWD/airootfs/etc/skel/.config/autostart/internet.sh
+sudo chmod +x $PWD/airootfs/etc/systemd/scripts/choose-mirror
+sudo chmod +x $PWD/airootfs/root/.automated_script.sh
+sudo chmod +x $PWD/airootfs/root/customize_airootfs.sh
+sudo chmod +x $PWD/airootfs/usr/bin/cnchi-start.sh
+sudo chmod +x $PWD/airootfs/usr/bin/internet.sh
 echo "DONE"
