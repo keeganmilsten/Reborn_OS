227588037d2ee6ebf9ec95be5c795aa013bf6685
Keegan <willnhalt@gmail.com>
2018-02-26 06:01:22 -0500

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,16 +1,13 @@
 #!/usr/bin/bash
 echo "Correcting file permissions"
-chmod +x ${work_dir}/Cnchi/postinstall.sh
-chmod +x ${work_dir}/Cnchi/10_antergos
-chmod +x ${work_dir}/Cnchi/pkcon.sh
-chmod +x ${work_dir}/Cnchi/pkcon2.sh
-chmod +x ${work_dir}/Cnchi/flatpak.sh
-chmod +x ${work_dir}/build.sh
-chmod +x ${work_dir}/translations.sh
-chmod +x ${work_dir}/airootfs/etc/skel/.config/autostart/internet.sh
-chmod +x ${work_dir}/airootfs/etc/systemd/scripts/choose-mirror
-chmod +x ${work_dir}/airootfs/root/.automated_script.sh
-chmod +x ${work_dir}/airootfs/root/customize_airootfs.sh
-chmod +x ${work_dir}/airootfs/usr/bin/cnchi-start.sh
-chmod +x ${work_dir}/airootfs/usr/bin/internet.sh
+chmod +x $PWD/Cnchi/postinstall.sh
+chmod +x $PWD/Cnchi/10_antergos
+chmod +x $PWD/build.sh
+chmod +x $PWD/translations.sh
+chmod +x $PWD/airootfs/etc/skel/.config/autostart/internet.sh
+chmod +x $PWD/airootfs/etc/systemd/scripts/choose-mirror
+chmod +x $PWD/airootfs/root/.automated_script.sh
+chmod +x $PWD/airootfs/root/customize_airootfs.sh
+chmod +x $PWD/airootfs/usr/bin/cnchi-start.sh
+chmod +x $PWD/airootfs/usr/bin/internet.sh
 echo "DONE"
